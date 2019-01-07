d5b9020b3cabb4bffa9199fd661c11886c2e9ce1
romontei <romontei@student.42.fr>
2019-01-07 22:08:23 +0100

    Corrections with new piscine subject.

--- d00/ex00/myawesomescript.sh
+++ d00/ex00/myawesomescript.sh
@@ -6,10 +6,10 @@
 ## Check if there is only one argument
 if [ $# = 1 ]; then
 
-	## Execute the curl command
+	## Execute the command
 	curl -Is $1 | grep 'Location' | cut -d' ' -f2
 
-## There is too less/much argument
+	## There is less/too much argument
 else
 	exit 1
 fi
